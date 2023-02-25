6dfc2e1fe0dec1a5e84c97480231557e190e1af1
yungblud <killi8n@gmail.com>
2023-02-25 22:43:36 +0900

    feat: added UI

---
+++ src/ui/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <p className={styles.title}>Giggle</p>
+    </div>
+  )
+}
+
+export default Header
