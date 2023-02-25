cca22656c501ebae9dd71fc2369bb45d7add5abd
yungblud <killi8n@gmail.com>
2023-02-25 23:19:12 +0900

    header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link'
 import styles from './header.module.css'
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <p className={styles.title}>Giggle</p>
+      <ul className={styles.menus}>
+        <li>
+          <Link href="/about">{"What's Giggle?"}</Link>
+        </li>
+        <li>
+          <Link className={styles.menusLogin} href="/login">
+            LOG IN
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
